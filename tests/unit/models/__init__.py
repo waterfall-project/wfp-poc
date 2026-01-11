0f099d6a7f15b8c75baf5b2a60c39297ba61938c
bengeek <bengeek06@gmail.com>
2026-01-11 18:56:02 +0100

    refactor: reorganize unit tests into models/schemas/resources subdirectories

---
+++ tests/unit/models/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2025 Waterfall
+#
+# This source code is dual-licensed under:
+# - GNU Affero General Public License v3.0 (AGPLv3) for open source use
+# - Commercial License for proprietary use
+#
+# See LICENSE and LICENSE.md files in the root directory for full license text.
+# For commercial licensing inquiries, contact: contact@waterfall-project.pro
+
+"""Unit tests for database models.
+
+This package contains all unit tests for SQLAlchemy models including
+validation, constraints, relationships, and business logic.
+"""
---
+++ tests/unit/schemas/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2025 Waterfall
+#
+# This source code is dual-licensed under:
+# - GNU Affero General Public License v3.0 (AGPLv3) for open source use
+# - Commercial License for proprietary use
+#
+# See LICENSE and LICENSE.md files in the root directory for full license text.
+# For commercial licensing inquiries, contact: contact@waterfall-project.pro
+
+"""Unit tests for Marshmallow schemas.
+
+This package contains all unit tests for schema validation,
+serialization, deserialization, and data transformation.
+"""
